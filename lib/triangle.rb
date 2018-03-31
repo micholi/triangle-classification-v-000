580dbe17baa117036730a98617f401c9879aa09b
Michelle Olivieri <michelleo72@gmail.com>
2018-03-31 22:09:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,10 @@ class Triangle
 
   end
 
+  def valid?
+
+  end
+
 # should raise a custom error, TriangleError if the triangle is invalid.
 # Check out the hint below to understand what makes a triangle invalid.
 # Write a custom error class, TriangleError and inherit it from StandardError.
